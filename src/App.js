67fdee3587f34324dd3e65e7722eeed4f8f0275c
Gladstone Rose <gladstonerose@hotmail.com>
2024-10-17 22:16:26 -0500

    modify App.js and add Footer.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ProjectInfoPage from './components/ProjectInfoPage';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
@@ -12,6 +13,8 @@ const App = () => {
           <Route exact path="/" element={<HomePage />} />
           <Route path="/project-info" element={<ProjectInfoPage />} />
         </Routes>
+        {/* Simple Copyright Footer */}
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Gladstone Rose (Quality Solutions Limited) Grass Growing Project. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
